04dcc825a78001f5a131afcafa4854cc3c0ddbd2
Andrew Yuen <yuen.andrew@epa.gov>
2021-10-05 15:25:25 -0400

    PATT-509 Change PATT to use 3 boxes per shelf, 10 shelves per bay PATT-516 Javascript 401 bug noticed when modifying the requested by name. PATT-517 Add ability to zip markdown files for help text. PATT-518 Add ability to list all markdown files in json format.

---
+++ scripts/json.php
@@ -0,0 +1,27 @@
+<?php
+header('Content-Type: application/json');
+
+$WP_PATH = implode("/", (explode("/", $_SERVER["PHP_SELF"], -6)));
+
+$dir = $_SERVER['DOCUMENT_ROOT'].$WP_PATH.'/web/app/helptext/content/pages';
+
+$list = array(); //main array
+
+if(is_dir($dir)){
+    if($dh = opendir($dir)){
+        while(($file = readdir($dh)) != false){
+
+            if($file == "." or $file == ".."){
+                //...
+            } else { //create object with two fields
+                $list3 = array(
+                'file' => $file);
+                array_push($list, $list3);
+            }
+        }
+    }
+
+    $return_array = array('files'=> $list);
+
+    echo json_encode($return_array);
+}
