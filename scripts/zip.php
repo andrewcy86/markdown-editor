9f52cfaaf6928384953be305110ec4ad9bafa545
Andrew Yuen <yuen.andrew@epa.gov>
2021-10-06 10:07:59 -0400

    Markdown editor zip file update

--- scripts/zip.php
+++ scripts/zip.php
@@ -3,47 +3,57 @@ $WP_PATH = implode("/", (explode("/", $_SERVER["PHP_SELF"], -6)));
 
 $dir = $_SERVER['DOCUMENT_ROOT'].$WP_PATH.'/app/helptext/content/pages';
 
-$zip_file = 'markdown_files.zip';
+$filename = 'markdown_files.zip';
 
-// Get real path for our folder
-$rootPath = realpath($dir);
-
-// Initialize archive object
-$zip = new ZipArchive();
-$zip->open($zip_file, ZipArchive::CREATE | ZipArchive::OVERWRITE);
-
-// Create recursive directory iterator
-/** @var SplFileInfo[] $files */
-$files = new RecursiveIteratorIterator(
-    new RecursiveDirectoryIterator($rootPath),
-    RecursiveIteratorIterator::LEAVES_ONLY
-);
-
-foreach ($files as $name => $file)
+function zip($source, $destination)
 {
-    // Skip directories (they would be added automatically)
-    if (!$file->isDir())
-    {
-        // Get real and relative path for current file
-        $filePath = $file->getRealPath();
-        $relativePath = substr($filePath, strlen($rootPath) + 1);
-
-        // Add current file to archive
-        $zip->addFile($filePath, $relativePath);
+    if (!extension_loaded('zip') || !file_exists($source)) {
+        return false;
     }
+
+    $zip = new ZipArchive();
+    if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+        return false;
+    }
+
+    $source = str_replace('\\', '/', realpath($source));
+
+    if (is_dir($source) === true) {
+        $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+        foreach ($files as $file) {
+            $file = str_replace('\\', '/', $file);
+
+            // Ignore "." and ".." folders
+            if (in_array(substr($file, strrpos($file, '/')+1), array('.', '..'))) {
+                continue;
+            }               
+
+            $file = realpath($file);
+
+            if (is_dir($file) === true) {
+                $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+            } elseif (is_file($file) === true) {
+                $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+            }
+        }
+    } elseif (is_file($source) === true) {
+        $zip->addFromString(basename($source), file_get_contents($source));
+    }
+
+    return $zip->close();
 }
 
-// Zip archive will be created only after closing object
-$zip->close();
+zip($dir, $filename);
 
+ if (file_exists($filename)) {
+  header('Content-Type: application/zip');
+  header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+  header('Content-Length: ' . filesize($filename));
 
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename='.basename($zip_file));
-header('Content-Transfer-Encoding: binary');
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize($zip_file));
-readfile($zip_file);
-unlink($zip_file);
+  flush();
+  readfile($filename);
+  // delete file
+  unlink($filename);
+
+ }
